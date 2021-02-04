27ec731a7d35f889dd1c7340454921a43b9140df
LeifTeorin <hjalle2000@gmail.com>
2021-02-04 09:34:17 +0100

    well here we are again

---
+++ Labb2/Labb2/main.c
@@ -0,0 +1,18 @@
+/*
+ * Labb2.c
+ *
+ * Created: 2021-02-04 09:33:58
+ * Author : hjall
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
